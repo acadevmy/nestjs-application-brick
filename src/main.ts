aa7807b1ce4ccab957ee8ab37fa1021b8219935c
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-05-27 12:30:07 +0200

    chore: init nestjs brick template

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('{{ applicationName }}')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3300);
+}
+bootstrap();
