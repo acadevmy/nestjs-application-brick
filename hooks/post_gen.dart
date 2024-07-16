6050f58b059b8621057e92be591d5c364c54763a
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-07-16 10:46:17 +0200

    fix: post gen message

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,15 +1,16 @@
 import 'package:mason/mason.dart';
+import 'package:chalkdart/chalk.dart';
 
 void run(HookContext context) async {
   logSentryConfiguration(context: context);
 }
 
 void logSentryConfiguration({required HookContext context}) {
-  context.logger.info(
-    '🛠️ To configure sentry, add values to the following environment variables:',
-  );
+  print(chalk.blue(
+    '🛠️ To configure Sentry, add values to the following environment variables:',
+  ));
 
   String applicationName = context.vars['applicationName'];
 
-  context.logger.write(' ${applicationName}_SENTRY_DNS');
+  print('   ${applicationName}_SENTRY_DNS\n');
 }
