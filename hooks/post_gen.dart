7906a42debc53386b9dc3f2d0a5f04545e3b0e37
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-07-16 10:35:46 +0200

    feat: add log sentry configuration

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,3 +1,15 @@
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {}
+void run(HookContext context) async {
+  logSentryConfiguration(context: context);
+}
+
+void logSentryConfiguration({required HookContext context}) {
+  context.logger.info(
+    '🛠️ To configure sentry, add values to the following environment variables:',
+  );
+
+  String applicationName = context.vars['applicationName'];
+
+  context.logger.write(' ${applicationName}_SENTRY_DNS');
+}
